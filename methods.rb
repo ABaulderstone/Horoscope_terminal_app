33ab66f0eda89debf86647109f403c125556683c
Alex Baulderstone <alexbaulderstone@gmail.com>
2019-04-01 13:24:11 +1100

    Split code into respective files

--- methods.rb
+++ methods.rb
@@ -1,3 +1,13 @@
 def validate_birthday(dob)
 !!dob[/\d{2}\/\d{2}\/\d{4}/]
 end
+
+def wait_clear
+    puts "."
+    sleep(1)
+    puts "."
+    sleep(1)
+    puts "."
+    sleep(1)
+    system "clear"
+end
